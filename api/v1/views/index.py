89f70b967c3e0a13509943c216841baeffc92d3e
ashrafbel <ashrafmus2@gmail.com>
2024-08-05 13:37:00 -0700

    status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,10 +9,11 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status')
 def status():
     "Returns status in JSON format"
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
