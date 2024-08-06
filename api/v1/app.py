512215ddc71cd4f293e23be6f016936fe8f0d46c
ashrafbel <ashrafmus2@gmail.com>
2024-08-05 19:30:27 -0700

    rv

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,8 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
